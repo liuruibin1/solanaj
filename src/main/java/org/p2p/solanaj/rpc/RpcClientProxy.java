0e08981c158900e527b1d037af78f0611d043378
liurb <1642515120@qq.com>
2024-04-08 18:01:58 +0800

    feat(rpc): update rpc proxy
    update rpc proxy

--- src/main/java/org/p2p/solanaj/rpc/RpcClientProxy.java
+++ src/main/java/org/p2p/solanaj/rpc/RpcClientProxy.java
@@ -1,12 +1,24 @@
 package org.p2p.solanaj.rpc;
 
 
-import okhttp3.OkHttpClient;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+import com.squareup.moshi.Types;
+import okhttp3.*;
+import org.p2p.solanaj.rpc.types.RpcRequest;
+import org.p2p.solanaj.rpc.types.RpcResponse;
 
+import javax.net.ssl.SSLHandshakeException;
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
+import java.util.List;
 
 public class RpcClientProxy extends RpcClient {
+
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
 
     private OkHttpClient httpClient;
 
@@ -30,5 +42,34 @@ public class RpcClientProxy extends RpcClient {
         }
         httpClient = clientBuilder.build();
         rpcApi = getApi();
+    }
+
+    public <T> T call(String method, List<Object> params, Class<T> clazz) throws RpcException {
+        RpcRequest rpcRequest = new RpcRequest(method, params);
+
+        JsonAdapter<RpcRequest> rpcRequestJsonAdapter = new Moshi.Builder().build().adapter(RpcRequest.class);
+        JsonAdapter<RpcResponse<T>> resultAdapter = new Moshi.Builder().build()
+                .adapter(Types.newParameterizedType(RpcResponse.class, Type.class.cast(clazz)));
+
+        Request request = new Request.Builder().url(getEndpoint())
+                .post(RequestBody.create(rpcRequestJsonAdapter.toJson(rpcRequest), JSON)).build();
+
+        try {
+            Response response = httpClient.newCall(request).execute();
+            final String result = response.body().string();
+            // System.out.println("Response = " + result);
+            RpcResponse<T> rpcResult = resultAdapter.fromJson(result);
+
+            if (rpcResult.getError() != null) {
+                throw new RpcException(rpcResult.getError().getMessage());
+            }
+
+            return (T) rpcResult.getResult();
+        } catch (SSLHandshakeException e) {
+            this.httpClient = new OkHttpClient.Builder().build();
+            throw new RpcException(e.getMessage());
+        } catch (IOException e) {
+            throw new RpcException(e.getMessage());
+        }
     }
 }
